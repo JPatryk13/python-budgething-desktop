507564dd99408cb3a541edb76e0263a871f89f46
jpatryk13 <jesionka.patryk13@gmail.com>
2023-09-16 04:20:47 +0100

    Moved table settings widgets to a separate class for better readibility

--- budgeting-app/budgeting_app/gui/services/table_extractor/table_extractor.py
+++ budgeting-app/budgeting_app/gui/services/table_extractor/table_extractor.py
@@ -8,29 +8,14 @@ from budgeting_app.gui.utils.tools import (
     create_spinbox_with_label,
     create_button_with_closeable_spinboxes
 )
-from budgeting_app.gui.services.table_extractor.image_tools import TableObjF, TableObjData, Tools
+from budgeting_app.gui.services.table_extractor.image_tools import QTableF, Tools, PythonicTableData
 from budgeting_app.gui.services.table_extractor.image_viewer import ImageViewer
 from budgeting_app.gui.services.base import MainWindow
 from budgeting_app.pdf_table_reader.core.entities.models import PDFFileWrapper
 from budgeting_app.pdf_table_reader.core.usecases.table_detector_workspace import TableDetectorWorkspace
 from budgeting_app.utils.tools import is_all_not_none
 
-class TableExtractor(MainWindow):
-
-    # editing_tools_widget hosts tool buttons and optionally table_size_widget
-    editing_tools_widget: QtWidgets.QWidget
-    editing_tools_layout: QtWidgets.QVBoxLayout
-
-    # tool buttons residing in editing_tools_widget
-    table_drawing_tool_button: QtWidgets.QPushButton
-    hand_tool_button: QtWidgets.QPushButton
-    
-    # the spinboxes that allow to adjust the number of columns and rows
-    table_size_widget: QtWidgets.QWidget
-    table_size_widget_active: bool
-    column_count_spinbox: QtWidgets.QSpinBox
-    row_count_spinbox: QtWidgets.QSpinBox
-    
+class TableSettingsWidgets:
     # table settings for pdfplumber
     vertical_strategy_widget: QtWidgets.QWidget
     horizontal_strategy_widget: QtWidgets.QWidget
@@ -54,165 +39,8 @@ class TableExtractor(MainWindow):
     intersection_tolerance_widget: QtWidgets.QWidget
     intersection_x_tolerance_widget: QtWidgets.QWidget
     intersection_y_tolerance_widget: QtWidgets.QWidget
-
-    # the pane that shows on the left and contains table
-    table_widget: QtWidgets.QTableWidget
-    # list of created tables /w (col_count, row_count)
-    tables: list[TableObjData]
-    # the img viewer
-    image_viewer: ImageViewer
-    # table detector workspace
-    table_detector_workspace: TableDetectorWorkspace
     
-
-    def __init__(self, parent: Any | None = None):
-        super().__init__(parent)
-        self.setWindowTitle("Budgeting App - Table Extractor")
-        # self.setGeometry(self.window_default_geometry)
-
-        self.tables = []
-
-        self.main_widget = QtWidgets.QWidget()
-        self.main_layout = QtWidgets.QHBoxLayout()
-        self.main_widget.setLayout(self.main_layout)
-        self.setCentralWidget(self.main_widget)
-
-        # Create a splitter for the table and image viewer
-        splitter = QtWidgets.QSplitter(QtCore.Qt.Orientation.Horizontal)
-
-        # Left widget (Table Widget)
-        self.table_widget = QtWidgets.QTableWidget(0, 0)
-        splitter.addWidget(self.table_widget)
-
-        # Right widget (Image Viewer)
-        self.image_viewer = ImageViewer()
-        splitter.addWidget(self.image_viewer)
-
-        self.main_layout.addWidget(splitter)
-
-        self._create_table_size_editor()
-        self._create_table_settings_widgets()
-        self._create_toolbar_widgets()
-        self._create_toolbar()
-        self._connect_to_image_viewer_signals()
-        
-    def set_table_detector_workspace(self, pdf_file_wrapper: PDFFileWrapper) -> None:
-        self.table_detector_workspace = TableDetectorWorkspace(pdf_file_wrapper)
-        self.image_viewer.set_image(self.table_detector_workspace.set_pdf_file_image().pdf_file.image.image_bytes)
-
-    def _create_toolbar(self):
-        toolbar = QtWidgets.QToolBar("Table Capture Tools", self)
-        self.addToolBar(QtCore.Qt.ToolBarArea.RightToolBarArea, toolbar)
-        toolbar.setMovable(False)
-        toolbar.addWidget(self.editing_tools_widget)
-        toolbar.addSeparator()
-        for w in [
-            self.vertical_strategy_widget,
-            self.horizontal_strategy_widget,
-            self.explicit_vertical_lines_widget,
-            self.explicit_horizontal_lines_widget,
-            self.snap_tolerance_widget,
-            self.snap_x_tolerance_widget,
-            self.snap_y_tolerance_widget,
-            self.join_tolerance_widget,
-            self.join_x_tolerance_widget,
-            self.join_y_tolerance_widget,
-            self.edge_min_length_widget,
-            self.min_words_vertical_widget,
-            self.min_words_horizontal_widget,
-            self.keep_blank_chars_widget,
-            self.text_tolerance_widget,
-            self.text_x_tolerance_widget,
-            self.text_y_tolerance_widget,
-            self.intersection_tolerance_widget,
-            self.intersection_x_tolerance_widget,
-            self.intersection_y_tolerance_widget
-        ]:
-            toolbar.addWidget(w)
-
-    def __table_drawing_tool_button_clicked(self) -> None:
-        if not self.table_drawing_tool_button.isChecked():
-            # Prevent unchecking the button
-            self.table_drawing_tool_button.setChecked(True)
-        else:
-            self.hand_tool_button.setChecked(False)
-
-        if not self.table_size_widget_active:
-            self._enable_table_size_editor()
-
-        self.image_viewer.set_tool(Tools.TABLE_DRAWING_TOOL)
-
-    def __hand_tool_button_clicked(self) -> None:
-        if not self.hand_tool_button.isChecked():
-            self.hand_tool_button.setChecked(True)
-        else:
-            self.table_drawing_tool_button.setChecked(False)
-
-        if self.table_size_widget_active:
-            self._enable_table_size_editor(False)
-
-        self.image_viewer.set_tool(Tools.HAND_TOOL)
-
-    def __column_count_changed(self) -> None:
-        self.image_viewer.set_col_count(self.column_count_spinbox.value())
-
-    def __row_count_changed(self) -> None:
-        self.image_viewer.set_row_count(self.row_count_spinbox.value())
-
-    def _create_table_size_editor(self) -> None:
-        self.table_size_widget = QtWidgets.QWidget()
-        table_size_layout = QtWidgets.QVBoxLayout()
-
-        line = QtWidgets.QFrame()
-        line.setFrameShape(QtWidgets.QFrame.Shape.HLine)
-        table_size_layout.addWidget(line)
-
-        column_count_widget, self.column_count_spinbox = create_spinbox_with_label("Columns: ", 2, 2, 30)
-        self.column_count_spinbox.valueChanged.connect(self.__column_count_changed)
-        table_size_layout.addWidget(column_count_widget)
-
-        row_count_widget, self.row_count_spinbox = create_spinbox_with_label("Rows: ", 3, 3, 50)
-        self.row_count_spinbox.valueChanged.connect(self.__row_count_changed)
-        table_size_layout.addWidget(row_count_widget)
-
-        self.table_size_widget.setLayout(table_size_layout)
-        self.table_size_widget_active = False
-
-    def _enable_table_size_editor(
-        self,
-        enable: bool = True,
-        col_count: int | None = None,
-        row_count: int | None = None
-    ) -> None:
-        self.table_size_widget_active = enable
-        if enable:
-            self.editing_tools_layout.addWidget(self.table_size_widget)
-            if is_all_not_none(col_count, row_count):
-                self.column_count_spinbox.setValue(col_count)
-                self.row_count_spinbox.setValue(row_count)
-        else:
-            self.editing_tools_layout.removeWidget(self.table_size_widget)
-    
-    def _create_toolbar_widgets(self) -> None:
-
-        self.editing_tools_widget = QtWidgets.QWidget()
-        self.editing_tools_layout = QtWidgets.QVBoxLayout()
-
-        self.table_drawing_tool_button = QtWidgets.QPushButton("Table Creator")
-        self.table_drawing_tool_button.setCheckable(True)
-        self.table_drawing_tool_button.setChecked(False)
-        self.table_drawing_tool_button.clicked.connect(self.__table_drawing_tool_button_clicked)
-        self.editing_tools_layout.addWidget(self.table_drawing_tool_button)
-
-        self.hand_tool_button = QtWidgets.QPushButton("Hand Tool")
-        self.hand_tool_button.setCheckable(True)
-        self.hand_tool_button.setChecked(True)
-        self.hand_tool_button.clicked.connect(self.__hand_tool_button_clicked)
-        self.editing_tools_layout.addWidget(self.hand_tool_button)
-
-        self.editing_tools_widget.setLayout(self.editing_tools_layout)
-
-    def _create_table_settings_widgets(self) -> None:
+    def __init__(self) -> None:
         self.vertical_strategy_widget, _ = create_dropdown_with_label(
             "Vertical strategy:",
             ["lines", "lines_strict", "text", "explicit"],
@@ -280,6 +108,189 @@ class TableExtractor(MainWindow):
 
         self.intersection_y_tolerance_widget, _ = create_spinbox_with_label("Intersection y tolerance: ", 0, 3)
 
+    def get_all_widgets(self) -> list[QtWidgets.QWidget]:
+        return [
+            self.vertical_strategy_widget,
+            self.horizontal_strategy_widget,
+            self.explicit_vertical_lines_widget,
+            self.explicit_horizontal_lines_widget,
+            self.snap_tolerance_widget,
+            self.snap_x_tolerance_widget,
+            self.snap_y_tolerance_widget,
+            self.join_tolerance_widget,
+            self.join_x_tolerance_widget,
+            self.join_y_tolerance_widget,
+            self.edge_min_length_widget,
+            self.min_words_vertical_widget,
+            self.min_words_horizontal_widget,
+            self.keep_blank_chars_widget,
+            self.text_tolerance_widget,
+            self.text_x_tolerance_widget,
+            self.text_y_tolerance_widget,
+            self.intersection_tolerance_widget,
+            self.intersection_x_tolerance_widget,
+            self.intersection_y_tolerance_widget
+        ]
+
+class TableExtractor(MainWindow):
+
+    # editing_tools_widget hosts tool buttons and optionally table_size_widget
+    editing_tools_widget: QtWidgets.QWidget
+    editing_tools_layout: QtWidgets.QVBoxLayout
+
+    # tool buttons residing in editing_tools_widget
+    table_drawing_tool_button: QtWidgets.QPushButton
+    hand_tool_button: QtWidgets.QPushButton
+    
+    # the spinboxes that allow to adjust the number of columns and rows
+    table_size_widget: QtWidgets.QWidget
+    table_size_widget_active: bool
+    column_count_spinbox: QtWidgets.QSpinBox
+    row_count_spinbox: QtWidgets.QSpinBox
+    
+    # table settings for pdfplumber
+    table_settings_widgets: TableSettingsWidgets
+
+    # the pane that shows on the left and contains table
+    table_widget: QtWidgets.QTableWidget
+    # list of created tables /w (col_count, row_count)
+    tables: list[PythonicTableData]
+    # the img viewer
+    image_viewer: ImageViewer
+    # table detector workspace
+    table_detector_workspace: TableDetectorWorkspace
+    
+
+    def __init__(self, parent: Any | None = None):
+        super().__init__(parent)
+        self.setWindowTitle("Budgeting App - Table Extractor")
+        # self.setGeometry(self.window_default_geometry)
+
+        self.tables = []
+
+        self.main_widget = QtWidgets.QWidget()
+        self.main_layout = QtWidgets.QHBoxLayout()
+        self.main_widget.setLayout(self.main_layout)
+        self.setCentralWidget(self.main_widget)
+
+        # Create a splitter for the table and image viewer
+        splitter = QtWidgets.QSplitter(QtCore.Qt.Orientation.Horizontal)
+
+        # Left widget (Table Widget)
+        self.table_widget = QtWidgets.QTableWidget(0, 0)
+        splitter.addWidget(self.table_widget)
+
+        # Right widget (Image Viewer)
+        self.image_viewer = ImageViewer()
+        splitter.addWidget(self.image_viewer)
+
+        self.main_layout.addWidget(splitter)
+        
+        self.table_settings_widgets = TableSettingsWidgets()
+
+        self._create_table_size_editor()
+        self._create_toolbar_widgets()
+        self._create_toolbar()
+        self._connect_to_image_viewer_signals()
+        
+    def set_table_detector_workspace(self, pdf_file_wrapper: PDFFileWrapper) -> None:
+        """Create TableDetectorWorkspace (pdfplumber table tools) with given pdf file wrapper object
+        and set the image wrapper object for it. Also, set the image for the image_viewer.
+        """
+        self.table_detector_workspace = TableDetectorWorkspace(pdf_file_wrapper)
+        self.image_viewer.set_images(self.table_detector_workspace.set_pdf_file_image().pdf_file.image.image_bytes)
+
+    def _create_toolbar(self):
+        toolbar = QtWidgets.QToolBar("Table Capture Tools", self)
+        self.addToolBar(QtCore.Qt.ToolBarArea.RightToolBarArea, toolbar)
+        toolbar.setMovable(False)
+        toolbar.addWidget(self.editing_tools_widget)
+        toolbar.addSeparator()
+        for w in self.table_settings_widgets.get_all_widgets():
+            toolbar.addWidget(w)
+
+    def __table_drawing_tool_button_clicked(self) -> None:
+        if not self.table_drawing_tool_button.isChecked():
+            # Prevent unchecking the button
+            self.table_drawing_tool_button.setChecked(True)
+        else:
+            self.hand_tool_button.setChecked(False)
+
+        if not self.table_size_widget_active:
+            self._enable_table_size_editor()
+
+        self.image_viewer.set_all_tabs_attr('drawing_enabled', True)
+
+    def __hand_tool_button_clicked(self) -> None:
+        if not self.hand_tool_button.isChecked():
+            self.hand_tool_button.setChecked(True)
+        else:
+            self.table_drawing_tool_button.setChecked(False)
+
+        if self.table_size_widget_active:
+            self._enable_table_size_editor(False)
+
+        self.image_viewer.set_all_tabs_attr('drawing_enabled', False)
+
+    def __column_count_changed(self) -> None:
+        self.image_viewer.set_all_tabs_attr('table_column_count', self.column_count_spinbox.value())
+
+    def __row_count_changed(self) -> None:
+        self.image_viewer.set_all_tabs_attr('table_row_count', self.row_count_spinbox.value())
+
+    def _create_table_size_editor(self) -> None:
+        self.table_size_widget = QtWidgets.QWidget()
+        table_size_layout = QtWidgets.QVBoxLayout()
+
+        line = QtWidgets.QFrame()
+        line.setFrameShape(QtWidgets.QFrame.Shape.HLine)
+        table_size_layout.addWidget(line)
+
+        column_count_widget, self.column_count_spinbox = create_spinbox_with_label("Columns: ", 2, 2, 30)
+        self.column_count_spinbox.valueChanged.connect(self.__column_count_changed)
+        table_size_layout.addWidget(column_count_widget)
+
+        row_count_widget, self.row_count_spinbox = create_spinbox_with_label("Rows: ", 3, 3, 50)
+        self.row_count_spinbox.valueChanged.connect(self.__row_count_changed)
+        table_size_layout.addWidget(row_count_widget)
+
+        self.table_size_widget.setLayout(table_size_layout)
+        self.table_size_widget_active = False
+
+    def _enable_table_size_editor(
+        self,
+        enable: bool = True,
+        col_count: int | None = None,
+        row_count: int | None = None
+    ) -> None:
+        self.table_size_widget_active = enable
+        if enable:
+            self.editing_tools_layout.addWidget(self.table_size_widget)
+            if is_all_not_none(col_count, row_count):
+                self.column_count_spinbox.setValue(col_count)
+                self.row_count_spinbox.setValue(row_count)
+        else:
+            self.editing_tools_layout.removeWidget(self.table_size_widget)
+    
+    def _create_toolbar_widgets(self) -> None:
+
+        self.editing_tools_widget = QtWidgets.QWidget()
+        self.editing_tools_layout = QtWidgets.QVBoxLayout()
+
+        self.table_drawing_tool_button = QtWidgets.QPushButton("Table Creator")
+        self.table_drawing_tool_button.setCheckable(True)
+        self.table_drawing_tool_button.setChecked(False)
+        self.table_drawing_tool_button.clicked.connect(self.__table_drawing_tool_button_clicked)
+        self.editing_tools_layout.addWidget(self.table_drawing_tool_button)
+
+        self.hand_tool_button = QtWidgets.QPushButton("Hand Tool")
+        self.hand_tool_button.setCheckable(True)
+        self.hand_tool_button.setChecked(True)
+        self.hand_tool_button.clicked.connect(self.__hand_tool_button_clicked)
+        self.editing_tools_layout.addWidget(self.hand_tool_button)
+
+        self.editing_tools_widget.setLayout(self.editing_tools_layout)
+
     def _connect_to_image_viewer_signals(self) -> None:
         # TODO: Update signals
         self.image_viewer.newTable.connect(self.__new_table)
@@ -292,7 +303,7 @@ class TableExtractor(MainWindow):
         self.table_widget.setColumnCount(max([len(table['vlines']) + 1 for table in self.tables]))
         self.table_widget.setRowCount(sum([len(table['hlines']) + 1 for table in self.tables]))
         
-    def __new_table(self, page_index: int, table_obj: TableObjF) -> None:
+    def __new_table(self, page_index: int, table_obj: QTableF) -> None:
         self.tables.append(table_obj.get_data())
             
         # Add table
@@ -306,7 +317,7 @@ class TableExtractor(MainWindow):
         
         self._update_table_widget()
 
-    def __table_updated(self, page_index: int, table_index: int, table_obj: TableObjF) -> None:
+    def __table_updated(self, page_index: int, table_index: int, table_obj: QTableF) -> None:
         # Remove old table
         self.table_detector_workspace.remove_table(
             top_left=self.tables[table_index]['top_left'],
