f380bb8ff7651764a38f1956940a2dcda4594f3c
jpatryk13 <jesionka.patryk13@gmail.com>
2023-09-19 19:37:34 +0100

    Added ExplicitLineData and adapted PDFPageWrapper to use TypedObservableDict

--- budgeting-app/budgeting_app/pdf_table_reader/core/entities/models.py
+++ budgeting-app/budgeting_app/pdf_table_reader/core/entities/models.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass, asdict, field
+import threading
 from typing import Literal
+from uuid import uuid4
 
-from pdfplumber import table, page
+from pdfplumber import page
+
+from budgeting_app.utils.types import TypedObservableDict
 
 BASE_IMAGE_RESOLUTION = 72
-
+            
 
 @dataclass
 class ImageWrapper:
@@ -13,13 +17,21 @@ class ImageWrapper:
     resolution: int = field(default=BASE_IMAGE_RESOLUTION)
     _format: str = field(default='PNG')
     antialias: bool = field(default=False)
-    
-    
+
+
+@dataclass
+class ExplicitLineData:
+    uuid: str = field(init=False, default_factory=lambda: str(uuid4()))
+    value: int | float
+    orientation: Literal['vertical', 'horizontal']
+
+
 @dataclass
 class PDFPageWrapper:
     page: page.Page
     base_size: tuple[int, int] = field(init=False)
-    table_settings: table.T_table_settings = field(default_factory=lambda: {})
+    table_settings: TypedObservableDict = field(init=False, default_factory=lambda: TypedObservableDict())
+    explicit_lines: list[ExplicitLineData] = field(default_factory=lambda: [])
     
     def __post_init__(self) -> None:
         self.base_size = self.page.to_image(BASE_IMAGE_RESOLUTION).original.size
