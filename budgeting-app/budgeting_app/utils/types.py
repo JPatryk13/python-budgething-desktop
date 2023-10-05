c71dc99abee00e0f8e6c667acc8d838bea0b5905
jpatryk13 <jesionka.patryk13@gmail.com>
2023-10-05 09:08:52 +0100

    Added logging for 'TypedObservableDict'

--- budgeting-app/budgeting_app/utils/types.py
+++ budgeting-app/budgeting_app/utils/types.py
@@ -1,7 +1,9 @@
+import logging
 import threading
-from typing import Any, Callable, TypeAlias, Generic, TypeVar
+from typing import Any, Callable, TypeAlias
 
 from pdfplumber._typing import T_point
+from budgeting_app.utils.logging import CustomLoggerAdapter
 
 T_uuid4_string: TypeAlias = str
 T_posix_timestamp: TypeAlias = int
@@ -13,24 +15,33 @@ T_raw_data: TypeAlias = list[str | list[str | None] | None]
 T_normalised_raw_data: TypeAlias = list[str]
             
 class TypedObservableDict(dict):
+    
+    logger = CustomLoggerAdapter.getLogger('app', className='TypedObservableDict')
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._callbacks: list[Callable[[str, Any], None]] = []
         self._lock = threading.Lock()
 
     def __setitem__(self, key: str, value: Any):
-        print(f'ObservableDict.__setitem__: key={key}, value={value}')
+        
+        self.logger.debug(f'Setting {key} to {value}.')
+        
         with self._lock:
             super().__setitem__(key, value)
             self._notify_observers(key, value)
 
     def add_callback(self, callback: Callable[[str, Any], None]):
-        print('ObservableDict.add_callback')
+        
+        self.logger.debug(f'Adding callback {callback}')
+        
         with self._lock:
             self._callbacks.append(callback)
 
     def _notify_observers(self, key: str, value: Any):
-        print(f'ObservableDict._notify_observers: key={key}, value={value}')
+        
+        self.logger.debug(f'Notifying observes that {key} has been changes.')
+        
         for callback in self._callbacks:
             callback(key, value)
             
